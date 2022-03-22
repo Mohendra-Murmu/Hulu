39661c24911c8fd0addef836a9b86e96cb9d8bf1
Darling <mahendradipu@gmail.com>
2022-03-23 00:21:39 +0530

    navComplete

--- utils/request.js
+++ utils/request.js
@@ -2,13 +2,21 @@
 const API_KEY = process.env.API_KEY;
 
 export default {
-    fetchTrending: {
+    fetchToprending: {
         title: 'Trending',
         url: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
     },
     fetchTopRated: {
         title: "Top Rated",
         url: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    },
+    fetchActionMovies: {
+        title: "Action",
+        url: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    },
+    fetchComedyMovies: {
+        title: "Comedy",
+        url: `/discovery/movie?api_key=${API_KEY}&with_genres=35`,
     },
 
 }
